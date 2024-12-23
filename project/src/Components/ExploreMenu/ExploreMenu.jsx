06c55b96b0b549033f171cf2fa74950c85221943
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2024-12-23 20:31:24 +0530

    Navbar and home components completed

---
+++ project/src/Components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './ExploreMenu.css'
+
+function ExploreMenu() {
+  return (
+    <div>ExploreMenu</div>
+  )
+}
+
+export default ExploreMenu
