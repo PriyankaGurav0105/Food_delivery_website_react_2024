059076299387118443a327a9da5917a428fa8722
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2024-12-24 00:01:24 +0530

    food display component is completed

--- project/src/pages/Home/Home.jsx
+++ project/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../Components/Header/Header'
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay';
 
 function Home() {
     const [category, setCategory] = useState("All");
@@ -11,6 +12,7 @@ function Home() {
     <div>
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
     </div>
   )
 }
