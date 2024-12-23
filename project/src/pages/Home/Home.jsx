35f0e784b22457af92cbbea4af0df7209109ca30
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2024-12-23 21:18:26 +0530

    Explore Menu component completed

--- project/src/pages/Home/Home.jsx
+++ project/src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../Components/Header/Header'
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
 
 function Home() {
+    const [category, setCategory] = useState("All");
+
   return (
     <div>
       <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
