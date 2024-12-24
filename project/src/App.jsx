3146e98ef7994eda0b9c1f709af539ea96b56804
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2024-12-24 22:48:22 +0530

    Footer component completed

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -5,9 +5,12 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Footer from './Components/Footer/Footer';
 
 const App = () => {
   return (
+    <>
+    
     <div className='app'>
       <Navbar/>
       <Routes>
@@ -16,6 +19,9 @@ const App = () => {
         <Route path='/order' element={<PlaceOrder/>}/>
       </Routes>
     </div>
+    <Footer/>
+    
+    </>
   )
 }
 
